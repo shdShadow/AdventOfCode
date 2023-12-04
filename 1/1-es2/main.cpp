7bb6d620c23361ad081fd959045d1f4fe96bf326
shdShadow <simomosino@gmail.com>
2023-12-04 23:51:19 +0100

    Deleted useless files

--- 1/1-es2/main.cpp
+++ 1/1-es2/main.cpp
@@ -3,11 +3,11 @@
 #include <string>
 #include <vector>
 using namespace std;
-int getNumberFromLiteral(string literal); 
+int getNumberFromLiteral(string literal);
 int main(int argc, char *argv[]) {
   string line = "";
   string literals[] = {"one", "two",   "three", "four", "five",
-                     "six", "seven", "eight", "nine"};
+                       "six", "seven", "eight", "nine"};
   ifstream file("input.txt");
   int sum = 0;
   int n = 0;
@@ -15,23 +15,24 @@ int main(int argc, char *argv[]) {
     while (getline(file, line)) {
       vector<int> numbers = vector<int>();
       for (int i = 0; i < line.length(); i++) {
-        n = int(line.at(i)) -48;
+        n = int(line.at(i)) - 48;
         if (n >= 1 && n <= 9) {
           numbers.push_back(n);
-        }else{
-          for (const auto &element : literals){
-            string substr = line.substr(i , element.length());
-            if (substr == element){
+        } else {
+          for (const auto &element : literals) {
+            string substr = line.substr(i, element.length());
+            if (substr == element) {
               int value = getNumberFromLiteral(element);
               numbers.push_back(value);
             }
           }
         }
       }
-              int n1 = numbers.at(0);
-        int n2 = numbers.at(numbers.size() -1);
-        sum += n1 * 10 + n2;
-        cout << "line: " << line << " n1: " << n1 <<" n2: " << n2 << " sum: " << sum << endl;
+      int n1 = numbers.at(0);
+      int n2 = numbers.at(numbers.size() - 1);
+      sum += n1 * 10 + n2;
+      cout << "line: " << line << " n1: " << n1 << " n2: " << n2
+           << " sum: " << sum << endl;
     }
   }
   return 0;
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]) {
 
 int getNumberFromLiteral(string literal) {
   string literals[] = {"one", "two",   "three", "four", "five",
-                     "six", "seven", "eight", "nine"};
+                       "six", "seven", "eight", "nine"};
   for (int i = 0; i < 9; i++) {
     if (literal == literals[i]) {
       return i + 1;
--- 1/1-es2/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-using namespace std;
-
-int main (int argc, char *argv[]) {
-  string line = "abcdefg";
-  cout << line.substr(1,3);
-  return 0;
-}
